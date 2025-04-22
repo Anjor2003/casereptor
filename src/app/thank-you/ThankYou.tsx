5cfaadad295e948ecbadd75aa454d3d01450b5e9
Anjor2003 <anjor2003@hotmail.com>
2025-04-22 19:31:03 +0200

    arreglos 2

--- src/app/thank-you/ThankYou.tsx
+++ src/app/thank-you/ThankYou.tsx
@@ -4,6 +4,7 @@ import { useQuery } from "@tanstack/react-query";
 import { getPaymentStatus } from "./actions";
 import { useSearchParams } from "next/navigation";
 import { Loader2 } from "lucide-react";
+import PhonePreview from "@/components/PhonePreview";
 
 const ThankYou = () => {
   const searchParams = useSearchParams();
@@ -39,18 +40,50 @@ const ThankYou = () => {
     );
   }
   const { configuration, billingAddress, shippingAddress, amount } = data;
-  const { color, } = configuration
+  const { color } = configuration;
 
-  return <div className="bg-white">
-    <div className="mx-auto max-w-3xl px-4 py-16 sm:px-6 sm:py-24 lg:px-8">
-      <div className="max-w-xl">
-        <p className="text-base font-medium text-primary">Gracias!</p>
-        <h1 className="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">
-          Tu carcasa esta en camino!
-        </h1>
+  return (
+    <div className="bg-white">
+      <div className="mx-auto max-w-4xl px-4 py-16 sm:px-6 sm:py-24 lg:px-8">
+        <div className="max-w-xl">
+          <p className="text-base font-medium text-primary">Gracias!</p>
+          <h1 className="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+            Tu carcasa esta en camino!
+          </h1>
+          <p className="mt-2 text-base text-zinc-500">
+            Hemos recibido tu pedido, y lo estamos procesando.
+          </p>
+
+          <div className="mt-12 text-sm font-medium">
+            <p className="text-zinc-900">Pedido Numero</p>
+            <p className="mt-2 text-zinc-500">{orderId}</p>
+          </div>
+        </div>
+
+        <div className="mt-10 border-t border-zinc-200">
+          <div className="mt-10 flex flex-auto flex-col">
+            <h4 className="font-semibold text-zinc-900">
+              Hiciste una gran eleccion!
+            </h4>
+            <p className="mt-2 text-sm text-zinc-600">
+              En Casereptor creemos que una funda de telefono personalizada es
+              el mejor regalo para un amigo o familia. Nosotros te ayudamos a
+              elegir la perfecta funda para ti. Te ayudaremos a elegir el
+              material, color y acabado para que te sientas orgulloso de tu
+              funda de telefono.
+            </p>
+          </div>
+        </div>
+
+        <div className="flex space-x-6 overflow-hidden mt-4 rounded-xl bg-gray-900/5 ring-1 ring-inset ring-gray-900/10 lg:rounded-2xl">
+          <PhonePreview
+            croppedImageUrl={configuration.croppedImageUrl!}
+            color={color!}
+          />
+        </div>
       </div>
     </div>
-  </div>;
+  );
 };
 
 export default ThankYou;
---
+++ src/components/PhonePreview.tsx
@@ -0,0 +1,53 @@
+"use client"
+
+import { CaseColor } from "@prisma/client"
+import { useRef, useState } from "react";
+import { AspectRatio } from "./ui/aspect-ratio";
+import { cn } from "@/lib/utils";
+
+const PhonePreview = ({
+  croppedImageUrl,
+  color,
+}: {
+  croppedImageUrl: string;
+  color: CaseColor;
+}) => {
+  const ref = useRef<HTMLDivElement>(null);
+  const [renderedDimension, setRenderedDimension] = useState({
+    width: 0,
+    height: 0,
+  });
+
+  let caseBackgroundColors = "bg-zinc-900"
+  if(color == 'blue') caseBackgroundColors = "bg-blue-950"
+  if(color == 'rose') caseBackgroundColors = "bg-rose-950"
+  if(color == 'orange') caseBackgroundColors = "bg-orange-950"
+  return (
+    <AspectRatio ref={ref} ratio={3000 / 2001} className="relative">
+      <div
+        className="absolute z-20 scale-[1.0352]"
+        style={{
+          left:
+            renderedDimension.width / 2 -
+            renderedDimension.width / (1216 / 121),
+          top: renderedDimension.height / 6.22,
+        }}>
+        <img
+          width={renderedDimension.width / (3000 / 671)}
+          src={croppedImageUrl}
+          alt="imagen cliente"
+          className={cn(
+            "phone-skew relative z-20 rounded-t-[15px] rounded-b-[10px] md:roundeed-t-[30px] md:rounded-b-[20px]",
+            caseBackgroundColors
+          )}
+        />
+      </div>
+
+      <div className="relative size-full z-40">
+        <img src="/clearphone.png" alt="phone" className="pointer-events-none size-full antialiased rounded-md" />  
+      </div>
+    </AspectRatio>
+  );
+};
+
+export default PhonePreview
