247eb80ff8bbae4ec27fcda9484aaa229f99b1bc
Anjor2003 <101336015+Anjor2003@users.noreply.github.com>
2025-04-24 17:28:18 +0200

    Update OrderReceivedEmail.tsx

--- src/components/emails/OrderReceivedEmail.tsx
+++ src/components/emails/OrderReceivedEmail.tsx
@@ -26,7 +26,7 @@ const OrderReceivedEmail = ({
   const baseUrl =
     process.env.NODE_ENV === "development"
       ? "http://localhost:3000"
-      : "https://casereptor.vercel.app/";
+      : "https://casereptor.vercel.app";
 
   return (
     <Html>
